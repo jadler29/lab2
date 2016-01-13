8b35a697215e2b46813f58ee334f8e801378e72d
jaadler29 <jonah@u.northwestern.edi>
2016-01-13 14:48:45 -0600

    lab2

--- src/lab2.cpp
+++ src/lab2.cpp
@@ -15,10 +15,65 @@ int illegalStatements() {
     //string mystring = 5;
     return 0;
 }
+void even() {
+
+    int oddeven;
+    cin >> oddeven;
+
+    if (oddeven%2)
+    {
+        cout << "odd" ;
+    }
+    else
+    {
+        cout << "even";
+    }
+
+}
+
+void calc()
+{
+    string operation;
+    double first;
+    double second;
+    cin >> operation >> first >> second;
+
+    if(operation == "+")
+    {
+        cout << (first+second);
+    }
+    if(operation == "-")
+    {
+        cout << (first - second);
+    }
+    if(operation == "*")
+    {
+        cout << (first*second);
+    }
+    if(operation == "")
+    {
+        cout << (first/second);
+    }
+
+}
+
+void friends()
+{
+string friend_gender;
+    enum friend_gender {male, female};
+    string friend_name;
+
+}
+
 
 int main() {
-    illegalStatements();
+
     cout << "Start with Exercise 1 (legal/illegal statements)" << endl;
     cout << "Then work on the exercises and code them up." << endl;
+   // even();
+    calc();
+
 }
+
+
 
